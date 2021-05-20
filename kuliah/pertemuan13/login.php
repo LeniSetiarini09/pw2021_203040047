cb79ab55adbe3cde1c3f0352c13f526b63da58f8
LeniSetiarini09 <leni.setiarini0903@gmail.com>
2021-05-20 13:32:03 +0700

    menambahkan fitur ajax dan upload gambar

---
+++ kuliah/pertemuan13/login.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ Leni Setiarini
+ 203040047
+ https://github.com/LeniSetiarini09/pw2021_203040047
+ Pertemuan 13 (14 Mei 2021)
+ Mempelajari mengenai (Ajax Live Search & Upload Gambar)
+ */
+?>
+
+
+<?php
+session_start();
+
+if (isset($_SESSION['login'])) {
+  header("Location: index.php");
+  exit;
+}
+
+require 'functions.php';
+
+// ketika tombol ditekan
+if (isset($_POST['login'])) {
+  $login = login($_POST);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+
+<body>
+  <h3>Form Login</h3>
+  <?php if (isset($login['error'])) : ?>
+    <p style="color: red; font-style:italic;"><?= $login['pesan']; ?></p>
+  <?php endif; ?>
+  <form action="" method="POST">
+    <ul>
+      <li>
+        <label>
+          Username :
+          <input type="text" name="username" autofocus autocomplete="off" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Password :
+          <input type="password" name="password" required>
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="login">Login</button>
+      </li>
+      <li>
+        <a href="registrasi.php">Tambah User Baru</a>
+      </li>
+    </ul>
+  </form>
+</body>
+
+</html>
